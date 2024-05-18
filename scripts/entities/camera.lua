e00fc2ee76a311c4aa9a7150a2d701064d1a6fbc
Kevin Thompson <email@kevinthompson.info>
2024-05-18 06:10:38 -0700

    chore: simplify camera logic to remove jitter

--- scripts/entities/camera.lua
+++ scripts/entities/camera.lua
@@ -1,16 +1,10 @@
 _camera = camera
 camera = entity:new({
-  x = player.x,
-  y = player.y,
-
-  hitbox = { -12, 12, -12, 12 },
+  layer = -10,
 
   draw = function(_ENV)
-    if not _ENV:entity_collide(x,y,player) then
-      local a = atan2(player.x - x, player.y - y)
-      x = mid(64, x + cos(a) * player.speed, 896)
-      y = mid(64, y + sin(a) * player.speed, 384)
-    end
+    x = mid(64, player.x, 896)
+    y = mid(64, player.y, 384)
 
     _camera(x - 64, y - 64)
   end
