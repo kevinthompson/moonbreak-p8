4399051b858936db9d86c505ad800f287cf5e078
Kevin Thompson <email@kevinthompson.info>
2024-05-22 12:07:25 -0700

    chore: procedurally draw spawner

--- scripts/entities/spawner.lua
+++ scripts/entities/spawner.lua
@@ -11,9 +11,15 @@ spawner = entity:extend({
   end,
 
   draw = function(_ENV)
-    spr(12,x-8,y-15,2,2)
-    if (open_percent == 50) sspr(112,8,8,2,x-3,y-14)
-    if (open_percent == 100) sspr(112,10,8,2,x-3,y-14)
+    spr(69,x-9,y-7)
+    spr(69,x+2,y-7,1,1,true)
+    circfill(x,y-8,7,7)
+
+    sspr(112,8,8,2,x-3,y-14)
+    if (open_percent == 50) sspr(112,10,8,2,x-3,y-14)
+    if (open_percent == 100) sspr(112,12,8,2,x-3,y-14)
+
+    spr(10,x-2,y-8)
   end,
 
   spawn_bot = function(_ENV)
