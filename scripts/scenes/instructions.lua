387fd30238b4ef846c8b5d9ef12d33491821312f
Kevin Thompson <email@kevinthompson.info>
2024-05-22 12:05:51 -0700

    chore: add basic instructions

--- scripts/scenes/instructions.lua
+++ scripts/scenes/instructions.lua
@@ -1,12 +1,42 @@
 instructions = scene:extend({
+  init = function(_ENV)
+    scene.init(_ENV)
+    controls_enabled = false
+    async:call(function()
+      wait(60)
+      controls_enabled = true
+    end)
+  end,
+
   update = function(_ENV)
-    if btnf(5) > 60 then
+    if controls_enabled and btnp(5) then
       scene:load(game)
     end
   end,
 
   draw = function(_ENV)
     cls(1)
-    printc("instructions",32,7)
+    printc("instructions",12,7)
+    line(32,20,96,20,5)
+
+
+    ? "\f7* \f6use arrow keys to walk", 16, 28
+    ? "  and aim", 16, 34
+
+    ? "\f7* \f6send a bot to collect", 16, 44
+    ? "  supplies by tapping \f7❎", 16, 50
+
+    ? "\f7* \f6recall your bot", 16, 60
+    ? "  by holding \f7❎", 16, 66
+
+    ? "\f7* \f6collecting supplies", 16, 76
+    ? "  creates more bots", 16, 82
+
+    ? "\f7* \f6use bots to collect", 16, 92
+    ? "  ship parts and leave", 16, 98
+
+    if controls_enabled then
+      printc("     \f7press \f9❎ \f7to start",112)
+    end
   end
 })
