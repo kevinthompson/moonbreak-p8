751bb243dad9e8e9ec4fa0aeb478df911cc27fad
Kevin Thompson <email@kevinthompson.info>
2024-05-22 16:56:01 -0700

    feat: make ship hitbox dynamic

--- scripts/entities/ship.lua
+++ scripts/entities/ship.lua
@@ -1,6 +1,12 @@
 ship = entity:extend({
   solid = true,
-  hitbox = {-20,19,-7,1},
+  hitbox = {-12,12,-7,1},
+  parts = {},
+
+  init = function(_ENV)
+    entity.init(_ENV)
+    parts = {}
+  end,
 
   draw = function(_ENV)
     spr(69,x-14,y-6)
@@ -11,5 +17,21 @@ ship = entity:extend({
 
   draw_shadow = function(_ENV)
     rectfill(x-12,y,x+11,y+2,14)
+  end,
+
+  add_part = function(_ENV,part)
+    if count(parts,part) == 0 then
+      _ENV:set_map_tiles(0)
+      add(parts,part)
+
+      if count(parts,cockpit) > 0 then
+        hitbox[2] = 19
+      end
+
+      if count(parts,booster) > 0 then
+        hitbox[1] = -20
+      end
+      _ENV:set_map_tiles(1)
+    end
   end
 })
